f8504d719317abd7e341f48994c8b38554c832c2
gguilhermelopes <guilhermelopes275@gmail.com>
2024-02-26 16:11:45 -0300

    feat: first commit

---
+++ src/main/java/com/gguilhermelopes/movieSphere/MovieSphereApplication.java
@@ -0,0 +1,13 @@
+package com.gguilhermelopes.movieSphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSphereApplication.class, args);
+	}
+
+}
